bc20f585cab9335f5bb2c88ab314466dd107867c
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-15 03:54:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,14 @@ def countdown(number)
   puts "#{number} SECOND(S)!"
   number -= 1
  end
- return "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number) 
-  while number > 0 do
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+     number -= 1
     sleep(number)
-    number -= 1
-  end
+   end
+   "HAPPY NEW YEAR!"
 end  
